489d325ae416e268edc4e23488918031eebb8559
Tamizheesh <152013781+Tamizheesh@users.noreply.github.com>
2023-11-25 12:55:48 -0500

    Added variables.py

---
+++ 01_variables.py
@@ -0,0 +1,82 @@
+####################variables##################
+#Rule1: Variable name must start with a letter or a underscore character
+             #Example: name="Tamizheesh"
+#Rule2:Variable name cannot start with a number
+               #Example: 1name="Tamizheesh"----> invalid variable
+#Rule3: Variable name can only contain alpha-numeric characters and underscores
+                #Example: Name_1="tamizheesh"
+#Rule4: Variable names are case sensitive
+               #Example:(hair,Hair,HAIR)
+#Rule5: variable name cannot contain any python keywords
+               #Example: if="Tamizheesh is remains alwys happy"-----> invalid variable
+               #Total keywords that are their in the python is: 33 keywords
+#programming starts
+
+age=12
+print(age)
+
+#check the type of variable
+print(type(age))
+
+weight=43.23
+print(weight)
+
+#check the type of the variable
+print(type(weight))
+
+name="TAMIZHEESH"
+print(name)
+
+# check the type of the variable
+print(type(name))
+
+height=173.33
+print(height)
+
+#check the types of the variables
+print(type(age))
+print(type(name))
+print(type(weight))
+print(type(weight))
+
+#boolean variable
+is_male=True
+print(is_male)
+
+#many values of multiple variables
+x,y,z="python","java","html"
+print(z,y,x)
+#########Note: number of values should match with number of variables##########
+#one value to multiple variables
+m=n=o=p=q=r=s=t=u=v=w=x=y=z="Tamizheesh is pro player in computer games and he is the god,of the roblox vedio games all will lose when i will play"
+print(m)
+print(n)
+print(o)
+print(p)
+print(q)
+print(r)
+print(s)
+print(t)
+print(u)
+print(v)
+print(w)
+print(x)
+print(y)
+print(z)
+#unpacked a collection
+fruits=["apple","mango","blue berry","red berry","papaya","strawberry","lichi","promogranate","starfruit","grapes","watermelon","muskmelon","banana","avocoda",]
+a,b,c,d,e,f,g,h,i,j,k,l,m,n=fruits
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
+print(m)
+print(n)
